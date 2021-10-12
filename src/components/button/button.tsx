d5912744e458ac4864413421c5769b33e3582523
ry928330 <ranyu938330@sina.com>
2021-10-12 10:10:08 +0800

    update buttonTypes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,14 +2,14 @@
  * @Author: ryyyyy
  * @Date: 2021-10-11 20:13:59
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-10-11 20:20:50
+ * @LastEditTime: 2021-10-12 10:09:48
  * @Description: Do not edit
  * @FilePath: /components/src/components/button/button.tsx
  */
 import * as React from 'react';
 import {tuple} from '../../_utils/type';
 
-const ButtonTypes = tuple('default', 'solid', 'link', 'text');
+const ButtonTypes = tuple('default', 'solid', 'link', 'text', 'primary');
 export type ButtonType = typeof ButtonTypes[number];
 
 export interface ButtonProps{
